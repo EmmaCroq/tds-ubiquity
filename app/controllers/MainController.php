c8f1fb2bdead5f43cfb4e57bd39f64cd91e95286
Emma Croq <21902471@etu.unicaen.fr>
2021-02-15 21:47:36 +0100

    feat(header, auth): add connect() in MyAuth and _userinfo in vHeader

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,8 +1,10 @@
 <?php
 namespace controllers;
+use models\User;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
+use Ubiquity\orm\DAO;
 
 /**
   * Controller MainController
@@ -12,11 +14,25 @@ class MainController extends ControllerBase{
 
     #[Route('_default',name:'home')]
 	public function index(){
-		$this->loadView('MainController/index.html');
+        $this->jquery->getHref('a[data-target]',
+        parameters: ['historize'=>false,'hasLoader'=>'internal', 'listenerOn'=>'body']);
+		$this->jquery->renderView('MainController/index.html');
 	}
 
     protected function getAuthController(): AuthController
     {
         return new MyAuth($this);
+    }
+
+    #[Route(path: "test/ajax",name: "main.testAjax")]
+    public function testAjax(){
+        $user=DAO::getById(User::class,[1],false);
+        $this->loadView('MainController/testAjax.html',['user'=>$user]);
+    }
+
+    #[Route('user/details/{id}',name: 'user.details')]
+    public function userDetails($id){
+        $user=DAO::getById(User::class,[$id],true);
+        echo "Organisation :".$user->getOrganization();
     }
 }
