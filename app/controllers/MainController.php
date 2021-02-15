73106ee9f123c45fa29b7f7d987697b1ab598003
Emma Croq <21902471@etu.unicaen.fr>
2021-02-15 23:31:52 +0100

    feat(ajax): add ajax features

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,38 +1,61 @@
 <?php
 namespace controllers;
+use models\Group;
 use models\User;
+use services\dao\OrgaRepository;
+use services\ui\UIGroups;
+use Ubiquity\attributes\items\di\Autowired;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
 use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\USession;
 
 /**
-  * Controller MainController
-  */
+ * Controller MainController
+ */
 class MainController extends ControllerBase{
     use WithAuthTrait;
 
-    #[Route('_default',name:'home')]
-	public function index(){
-        $this->jquery->getHref('a[data-target]',
-        parameters: ['historize'=>false,'hasLoader'=>'internal', 'listenerOn'=>'body']);
-		$this->jquery->renderView('MainController/index.html');
-	}
+    #[Autowired]
+    private OrgaRepository $repo;
+    private UIGroups $uiService;
 
-    protected function getAuthController(): AuthController
-    {
+    public function initialize(){
+        parent::initialize();
+        $this->uiService=new UIGroups($this);
+    }
+
+
+    public function setRepo(OrgaRepository $repo): void {
+        $this->repo = $repo;
+    }
+
+    #[Route('_default',name:'home')]
+    public function index(){
+        $this->jquery->renderView("MainController/index.html");
+    }
+
+    protected function getAuthController(): AuthController{
         return new MyAuth($this);
     }
 
     #[Route(path: "test/ajax",name: "main.testAjax")]
     public function testAjax(){
         $user=DAO::getById(User::class,[1],false);
-        $this->loadView('MainController/testAjax.html',['user'=>$user]);
+        $this->loadDefaultView(['user'=>$user]);
     }
 
-    #[Route('user/details/{id}',name: 'user.details')]
+    #[Route('user/details/',name:'user.details')]
     public function userDetails($id){
         $user=DAO::getById(User::class,[$id],true);
-        echo "Organisation :".$user->getOrganization();
+        echo "Organisation : ".$user->getOrganization();
+    }
+    #[Route('groups/list',name: 'groups.list')]
+    public function listGroups(){
+        $idOrga=USession::get('idOrga');
+        $groups=DAO::getAll(Group::class,'idOrganization= ?',false,[$idOrga]);
+        $this->uiService->listGroups($groups);
+        $this->jquery->renderDefaultView();
     }
 }
