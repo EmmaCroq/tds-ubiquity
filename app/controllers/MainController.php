a8b414b468afee5edce64e39a59a750951179a0f
Emma Croq <21902471@etu.unicaen.fr>
2021-03-21 15:17:34 +0100

    fix(): fix issue with images and DAO::count

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -23,9 +23,9 @@ class MainController extends ControllerBase{
 
     #[Route('_default',name:'home')]
 	public function index(){
-        $nbOrders = count(DAO::getAll(Order::class, 'idUser= ?', false, [USession::get("idUser")]));
+        $nbOrders = DAO::count(Order::class, 'idUser= ?', [USession::get("idUser")]); // not count(DAO::getAll
         $listProm = DAO::getAll(Product::class, 'promotion< ?', false, [0]);
-        $nbBaskets = count(DAO::getAll(Basket::class, 'idUser= ?', false, [USession::get("idUser")]));
+        $nbBaskets = DAO::count(Basket::class, 'idUser= ?', [USession::get("idUser")]);
         $this->loadDefaultView(['nbOrders'=>$nbOrders, 'listProm'=>$listProm, 'nbBaskets'=>$nbBaskets]);
 	}
 
@@ -52,7 +52,7 @@ class MainController extends ControllerBase{
         $store = DAO::getAll(Product::class, false, false);
         $listSection = DAO::getAll(Section::class, false, false);
         $listProm = DAO::getAll(Product::class, 'promotion< ?', false, [0]);
-        //$nbprodSection = count(DAO::getAll(Product::class, 'section= ?', false));
+        //$nbprodSection = DAO::count(Product::class, 'section= ?', false?);
         $this->loadDefaultView(['store'=>$store, 'listSection'=>$listSection, 'listProm'=>$listProm]);
     }
 
